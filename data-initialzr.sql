855af6e9c909ca08f310270de0e56c5b40c59fad
AshkanIzadpanah <ashkan@Ashkans-MacBook-Pro.local>
2021-01-04 23:22:20 -0500

    data-inituialzr

--- data-initialzr.sql
+++ data-initialzr.sql
@@ -1,14 +1,22 @@
 use lms;
+delete from book;
 delete from course;
 delete from topic;
 
+insert into topic (id, description, name, details) values (1, 'containarization including docker and kubernetes','Containers', null);
+insert into topic (id, description, name, details) values (2, 'Java Core libraries - collections, streams, concurrent','Java Core', null);
+insert into topic (id, description, name, details) values (3, 'Spring framework - Core/MVC/Data/Security & springBoot','Spring', null);
+insert into topic (id, description, name, details) values (4, 'Front end technologies - javascript, typescript, AngularJs, RxJs, HTML 5.0, CSS','Frontend', null);
+insert into topic (id, description, name, details) values (5, 'Distributed systems and microservices','System design', null);
 
-insert into topic values ('containers', 'containarization including docker and kubernetes','containers');
-insert into topic values ('Java Core', 'Java Core libraries - collections, streams, concurrent','Java Core');
-insert into topic values ('Spring', 'Spring framework - Core/MVC/Data/Security & springBoot','Spring');
-insert into topic values ('Frontend', 'Front end technologies - javascript, typescript, AngularJs, RxJs, HTML 5.0, CSS','Frontend');
+insert into course (id, description, title, recommended_book_id, topic_id) values (1, 'k8s for Beginners [FULL COURSE in 4 Hours]', 'Kubernetes Tutorial', null, 1);
+insert into course (id, description, title, recommended_book_id, topic_id) values (2, 'Java Collections Streams Excutorservice JDK8','Java Core', null, 2);
+insert into course (id, description, title, recommended_book_id, topic_id) values (3, 'Spring Boot MVC Data Actuator','Spring Boot Quick Start', null, 3);
+insert into course (id, description, title, recommended_book_id, topic_id) values (4, 'Angular 7 CRUD Operation', 'Angular 7 CRUD, Service, Components, class models ', null, 4);
+insert into course (id, description, title, recommended_book_id, topic_id) values (5, 'Google Certified Associate Cloud Engineer', 'Udemy course, hands on with Google Cloud Platform (GCP)', null, 5);
 
-insert into course values ('k8s', 'kubernetes 101','containers');
-insert into course values ('javaCollections', 'Java Collections','Java Core');
-insert into course values ('SpringBoot', 'Spring Boot applications','Spring');
-insert into course values ('Angular', 'Angular 11 CRUD, Service, Components ','Frontend');
+insert into book (id, authur, description, isb, online_resource, title,  url_link, topic_id) values(1, 'Bijith NV', '', '', 0, 'Angular CRUD', 'https://www.youtube.com/watch?v=6m45I75RD_M', 4);
+insert into book (id, authur, description, isb, online_resource, title,  url_link, topic_id) values(2, 'Mosh', '', '', 0, 'Angular Tutorial for Beginners', 'https://www.youtube.com/watch?v=k5E2AVpwsko', 4);
+insert into book (id, authur, description, isb, online_resource, title,  url_link, topic_id) values(3, 'DesignCourse', '', '', 0, 'Learn Angular 7 in 50 Minutes', 'https://www.youtube.com/watch?v=5wtnKulcquA', 4);
+insert into book (id, authur, description, isb, online_resource, title,  url_link, topic_id) values(4, 'Travery Media', '', '', 0, 'Angular In 60 Minutes', 'https://www.youtube.com/watch?v=KhzGSHNhnbI', 4);
+  
